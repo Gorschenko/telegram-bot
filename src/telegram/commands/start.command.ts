a15ab8d76fe3a922d676dc4ec4a208591c392823
Alexey <aa.gorschenko@yandex.ru>
2023-03-10 12:32:10 +0300

    TelegramModule. StartCommand has been created

---
+++ src/telegram/commands/start.command.ts
@@ -0,0 +1,31 @@
+import { Markup, Telegraf } from 'telegraf';
+import { IBotContext } from '../context/context.interface';
+import { Command } from './command.class';
+
+export class StartCommand extends Command {
+  constructor(bot: Telegraf<IBotContext>) {
+    super(bot);
+  }
+
+  handle(): void {
+    this.bot.start((ctx) => {
+      ctx.reply(
+        'Вам понравился крус?',
+        Markup.inlineKeyboard([
+          Markup.button.callback('Да', 'course_like'),
+          Markup.button.callback('Нет', 'course_dislike'),
+        ]),
+      );
+    });
+
+    this.bot.action('course_like', (ctx) => {
+      ctx.session.courseLike = true;
+      ctx.editMessageText('Круто!');
+    });
+
+    this.bot.action('course_dislike', (ctx) => {
+      ctx.session.courseLike = false;
+      ctx.editMessageText('Мы будем лучше!');
+    });
+  }
+}
