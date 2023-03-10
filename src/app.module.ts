f6a5f25182dca72bf80050a9f971a6a62bdc63a6
Alexey <aa.gorschenko@yandex.ru>
2023-03-10 10:58:04 +0300

    TelegramModule. forRootAsyn method has been added for

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config/dist';
+import { getTelegramConfig } from './configs/telegram.config';
 import { TelegramModule } from './telegram/telegram.module';
 import { TestModule } from './test/test.module';
 
 @Module({
-  imports: [TelegramModule, TestModule],
+  imports: [
+    TestModule,
+    TelegramModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getTelegramConfig,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
